c2c74378fa9432b06ca2c10e29d50cf9558c0288
kclarkedesign <kclarke.design@gmail.com>
2018-06-04 18:10:51 -0400

    added server.js and Database Table Properties for migration.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express  = require('express');
+const app = express();
+
+//middleware for CORS requests
+const cors = require('cors');
+app.use(cors());
+
+//middleware for parsing request bodies
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+//Development-only error handler middleware.
+const errorhandler = require('errorhandler');
+if (process.env.NODE_ENV === 'development') {
+	app.use(errorhandler());
+}
+
+//middleware for logging
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
+});
+
+const apiRouter = require('./api/api');
+app.use('/api', apiRouter);
+
+
+module.exports = app;
