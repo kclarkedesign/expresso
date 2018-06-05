5c556a7222ffd5a1caa65022d1b7a8c38854bce6
Kirk Clarke <kclarke.design@gmail.com>
2018-06-05 06:53:10 -0400

    Added server dependencies, and started building out api route files

--- api/employees.js
+++ api/employees.js
@@ -14,3 +14,42 @@ employeesRouter.get('/', (res, req, next) => {
 		}
 	});
 });
+
+employeesRouter.post('/', (req, res, next) => {
+	const employee = req.body.employee;
+	const name;
+	const position;
+	const wage;
+	const isCurrentEmployee = employee.isCurrentEmployee === 0 ? 0 : 1;
+
+	if(!name || !position || !wage) {
+		return res.sendStatus(400);
+	}
+
+	const query = `INSERT INTO Employee (
+					name,
+					position,
+					wage,
+					is_current_employee)
+				Values (
+					$name,
+					$position,
+					$wage,
+					$isCurrentEmployee)`;
+	const values = {
+		$name: name,
+		$position: position,
+		$wage: wage,
+		$isCurrentEmployee: isCurrentEmployee
+	};
+
+	db.run(query, values, function(error) {
+		if(error) {
+			next(error);
+		} else {
+			db.get(`SELECT * FROM Employee WHERE id = ${this.lastID}`, function(error, employee) {
+				res.status(201).json({ employee: employee });
+			});
+		}
+	})
+});
